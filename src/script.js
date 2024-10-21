1942f9c9f81990ea41bd9158ecf1e16611d22123
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2024-10-21 15:55:55 +0800

    added a script.js file and added some functionality to login.html

---
+++ src/script.js
@@ -0,0 +1,19 @@
+const login = document.getElementById("loginForm");
+
+login.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("email");
+    const passInput = document.getElementById("password")
+
+    const email = emailInput.value;
+    const password = passInput.value;
+
+    console.log(email + "   " + password);
+
+    if (email == "admin@admin.com" && password == "admin") {
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Why is it wrong");
+    }
+});
