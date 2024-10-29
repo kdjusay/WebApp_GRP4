4413d2b7460f7db4e5797f9fd89bde259e268180
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2024-10-29 19:42:27 +0800

    Addded initial Animations and button

--- src/script.js
+++ src/script.js
@@ -19,3 +19,4 @@ login.addEventListener("submit", function(event) {
 
 
 });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {}
+    extend: {
+      safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]']
+    }
   },
   plugins: [
     require('flowbite/plugin')
