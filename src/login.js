545910f1a821b4237ff21e0e43d2f53c280bb3d0
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2024-11-05 14:10:31 +0800

    fixed login

--- src/login.js
+++ src/login.js
@@ -1,23 +1,21 @@
-    const login = document.getElementById("loginForm");
-    
-    login.addEventListener("DOMContentLoaded", (event) => {
-        event.preventDefault();
-        
-        const emailInput = document.getElementById("email");
-        const passInput = document.getElementById("password")
-    
-        const email = emailInput.value;
-        const password = passInput.value;
-    
-        console.log(email + "   " + password);
-    
-        if (email == "admin@admin.com" && password == "admin") {
-            
-            window.location.href = "index.html";
-        } else {
-            alert("Why is it wrong");
-        }
-    
-    
-    });
-    
+const login = document.getElementById("loginForm");
+
+  login.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("email");
+    const passInput = document.getElementById("password")
+
+    const email = emailInput.value;
+    const password = passInput.value;
+
+    console.log(email + "   " + password);
+
+    if (email == "admin@admin.com" && password == "admin") {
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Why is it wrong");
+    }
+
+
+});
