b819cc4081ee072f1004f423f2e680da9370a36e
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2024-11-05 13:27:10 +0800

    Added another js for login

--- src/script.js
+++ src/script.js
@@ -12,26 +12,3 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
-
-const login = document.getElementById("loginForm");
-
-login.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const emailInput = document.getElementById("email");
-    const passInput = document.getElementById("password")
-
-    const email = emailInput.value;
-    const password = passInput.value;
-
-    console.log(email + "   " + password);
-
-    if (email == "admin@admin.com" && password == "admin") {
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Why is it wrong");
-    }
-
-
-});
-
