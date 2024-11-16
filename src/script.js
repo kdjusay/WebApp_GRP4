2d7e8b49823e2c792c27cd07040c65d233fb94bb
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2024-11-16 15:42:06 +0800

    Added file upload interface

--- src/script.js
+++ src/script.js
@@ -10,23 +10,6 @@ mathLinks.forEach(link => {
   });
 });
 
-  const fName = document.getElementsByName('fname')
-  const LName = document.getElementById('last_name');
-  const email = document.getElementById('email');
-  const password = document.getElementById('password');
-  const daButton = document.getElementById('updateButton');
-
-
-  daButton.addEventListener('click', () => {
-    updatePlaceholder('fname', 'yay text')
-  })
-
-  function updatePlaceholder() {
-    const inputelement = document.getElementById('inputId');
-    inputelement.setAttribute('ariaPlaceholder', 'New textttt');
-  }
-
-
 function todocuments() {
 document.getElementById("theFrame").src = "/src/navbar/documents.html";
 }
